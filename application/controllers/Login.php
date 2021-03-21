c5aaf6a09f6f469fe839c0b03221d246be86f302
Arturo Sarango <arturo_ajsa@hotmail.com>
2021-03-20 17:50:35 -0700

    Arreglo de login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,50 +1,52 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require APPPATH.'/libraries/REST_Controller.php';
+require APPPATH . '/libraries/REST_Controller.php';
 header('Access-Control-Allow-Origin: *');
 
-if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-	header('Access-Control-Allow-Methods: POST');
-	header('Access-Control-Allow-Headers: Content-Type');
-	exit;
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Content-Type');
+    exit;
 }
 
 class Login extends REST_Controller {
-    
+
     public function __construct() {
         //llamando a constructor padre
         parent::__construct();
         $this->load->database(); //pegandose a la base
         $this->load->model('User_model'); //cargando el modelo User
-        
     }
-    
-     
+
     /**
      * Metodo que entrega todos los registros academicos del docente
      * url del servicio: http://192.168.100.150/codigneiter/index.php/Registrosacademicos/registros/ZG9jZWluaTFAbWFpbC5jb20yMDIxMDMwMjE3Mjk0MA==
      */
-    public function login_post(){
-        if(json_decode(file_get_contents('php://input'), true)){
+    public function login_post() {
+        if (json_decode(file_get_contents('php://input'), true)) {
             $data = json_decode(file_get_contents('php://input'), true);
-        }else{
+        } else {
             $data = $this->post();
         }
-        
+
         $login = $this->User_model->login($data);
-        
-        $respuesta = array(
-            'error'     => false,
-            'message'   => 'Conexión correcta',
-            'data'      => $login
-        );
-        
+
+        if ($login == false) {
+            $respuesta = array(
+                'error' => true,
+                'message' => 'Usuario y/o clave incorrecto!!!',
+                'data' => null
+            );
+        } else {
+            $respuesta = array(
+                'error' => false,
+                'message' => 'Conexión correcta',
+                'data' => $login
+            );
+        }
         $this->response($respuesta);
-        
     }
-    
-    
-    
-    
+
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,49 +2,58 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User_model extends CI_Model{
-    
+class User_model extends CI_Model {
+
     private $username;
     private $password;
-    
-    /**********
+
+    /*     * ********
      * (1)
      * METODO QUE RECIBE INFORMACION DE USUARIO PARA REALIZAR LOGIN
      * 
      */
-    public function login($dataPost){
-        
+
+    public function login($dataPost) {
+
         $username = $dataPost['username'];
         $pass = $dataPost['password'];
-        
-        $password = md5($pass);
-        
-        
-        $sql = 'SELECT seguridad."ValidarUsuarioClave"('."'$username','$password','permisos','empresas','estado');";
-        
-        /**** para tomar cursores de consulta de login*////
-        pg_query("BEGIN;");
-        $procAlmacenado    = pg_query($sql);
-        $respuesta         = pg_fetch_all($procAlmacenado);
 
-        $respEstado        = pg_query("FETCH ALL estado;");
-        $respPermisos      = pg_query("FETCH ALL permisos;");
-        $respEmpresas      = pg_query("FETCH ALL empresas;");
-        
-        $estado            = pg_fetch_all($respEstado);
-        $permisos          = pg_fetch_all($respPermisos);
-        $empresas          = pg_fetch_all($respEmpresas);
-        
-        pg_query("END;");
-        
-        $estado     ? $estado   = $estado   : $estado   = null;
-        $permisos   ? $permisos = $permisos : $permisos = null;
-        $empresas   ? $empresas = $empresas : $empresas = null;
-        
-        return array(
-            'estado'    => $estado,
-            'permisos'  => $permisos,
-            'empresas'  => $empresas
+        $password = md5($pass);
+
+
+        $sql = 'SELECT seguridad."ValidarUsuarioClave"(' . "'$username','$password','permisos','empresas','estado');";
+
+        /*         * ** para tomar cursores de consulta de login *////
+        pg_query("BEGIN;");
+        $procAlmacenado = pg_query($sql);
+        $respuesta = pg_fetch_all($procAlmacenado);
+
+        $respEstado = pg_query("FETCH ALL estado;");
+        $estado = pg_fetch_all($respEstado);
+
+        if ($estado[0]['coderror'] == 'ERROR') {
+          $respuesta = false;  
+        } else {
+            $respPermisos = pg_query("FETCH ALL permisos;");
+            $respEmpresas = pg_query("FETCH ALL empresas;");
+            $permisos = pg_fetch_all($respPermisos);
+            $empresas = pg_fetch_all($respEmpresas);
+            
+            $estado     ? $estado   = $estado   : $estado   = null;
+            $permisos   ? $permisos = $permisos : $permisos = null;
+            $empresas   ? $empresas = $empresas : $empresas = null;
+            
+            $respuesta = array(
+            'estado' => $estado,
+            'permisos' => $permisos,
+            'empresas' => $empresas
         );
+            
+        }
+        pg_query("END;");
+
+
+        return $respuesta; 
     }
+
 }
